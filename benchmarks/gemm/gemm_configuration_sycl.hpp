aa67e8a0922823deeb7d2b67814982650991e051
Deng, Jacky <jacky.deng@intel.com>
2025-05-07 13:35:56 +0800

    Added the second release required fine-tuned GEMM kernels.

--- benchmarks/gemm/gemm_configuration_sycl.hpp
+++ benchmarks/gemm/gemm_configuration_sycl.hpp
@@ -149,6 +149,87 @@ struct GemmConfiguration<
   }
 };
 
+/////////////////////////////////////////////////////////////////////////
+
+// float16
+
+template<typename LayoutA, typename LayoutB, typename LayoutC,
+  class TileShape, class TiledMma, class GmemTiledCopyA, class GmemTiledCopyB, Scheduler TileScheduler, class EpilogueOp>
+struct GemmConfiguration<
+      arch::IntelXe,
+      cutlass::half_t, LayoutA,
+      cutlass::half_t, LayoutB,
+      float, LayoutC,
+      float, TileShape, TiledMma,
+      GmemTiledCopyA, GmemTiledCopyB, TileScheduler, EpilogueOp> {
+  using DispatchPolicy = MainloopIntelXeXMX16<3, std::conditional_t<TileScheduler == Scheduler::Gemm, cutlass::gemm::KernelXe, cutlass::gemm::KernelXeCooperative>>;
+
+  // Configurations in benchmarks.hpp can pass either a layout tag (e.g. RowMajor) or a Stride directly
+  using StrideA = std::conditional_t<cute::is_tuple_v<LayoutA>, LayoutA, TagToStrideA_t<LayoutA>>;
+  using StrideB = std::conditional_t<cute::is_tuple_v<LayoutB>, LayoutB, TagToStrideB_t<LayoutB>>;
+  using StrideC = std::conditional_t<cute::is_tuple_v<LayoutC>, LayoutC, TagToStrideC_t<LayoutC>>;
+
+  // Mainloop
+  using CollectiveMainloop = collective::CollectiveMma<
+    DispatchPolicy, TileShape,
+    cutlass::half_t, StrideA,
+    cutlass::half_t, StrideB,
+    TiledMma,
+    GmemTiledCopyA, void, void, identity, // A
+    GmemTiledCopyB, void, void, identity // B
+  >;
+
+  // Epilogue
+  using EpilogueDispatchPolicy = epilogue::IntelXeXMX16;
+
+  // TODO(codeplay): Refactor this following Testbed3x approach. See benchmark_runner.hpp
+  using FusionCallBacks = std::conditional_t<
+      EpilogueOp::IsAuxInSupported, // ~Is mul_add
+      epilogue::fusion::FusionCallbacks<EpilogueDispatchPolicy, EpilogueOp, TileShape,
+                                        decltype(tile_shape(TiledMma())), XE_2D_U32x8x16_LD_N>,
+      epilogue::fusion::FusionCallbacks<EpilogueDispatchPolicy, EpilogueOp, TileShape,
+                                        decltype(tile_shape(TiledMma()))>>;
+
+  using CollectiveEpilogue = epilogue::collective::CollectiveEpilogue<
+        EpilogueDispatchPolicy,
+        TileShape,
+        float,
+        StrideC,
+        float,
+        StrideC,
+        FusionCallBacks,
+        XE_2D_U32x8x16_LD_N,
+        void, void,
+        XE_2D_U32x8x16_ST_N,
+        void, void>;
+
+  using GemmKernel = kernel::GemmUniversal<
+    Shape<int, int, int, int>,
+    CollectiveMainloop,
+    CollectiveEpilogue,
+    std::conditional_t<TileScheduler == Scheduler::Gemm, void, cutlass::gemm::StreamKScheduler>
+  >;
+
+  using Gemm = GemmUniversalAdapter<GemmKernel>;
+
+  constexpr static typename GemmKernel::Arguments defaultArguments() {
+    using StreamKMode =
+      cutlass::gemm::kernel::detail::PersistentTileSchedulerXeStreamKParams::DecompositionMode;
+    if constexpr (TileScheduler == Scheduler::Gemm) {
+      return {};
+    } else if constexpr (TileScheduler == Scheduler::GemmStreamK) {
+      typename GemmKernel::Arguments arguments{};
+      arguments.scheduler = {1, StreamKMode::StreamK};
+      return arguments;
+    } else {
+      static_assert(TileScheduler == Scheduler::GemmSplitK);
+      typename GemmKernel::Arguments arguments{};
+      arguments.scheduler = {2, StreamKMode::SplitK};
+      return arguments;
+    }
+  }
+};
+
 } // namespace device
 } // namespace gemm
 } // namespace cutlass
